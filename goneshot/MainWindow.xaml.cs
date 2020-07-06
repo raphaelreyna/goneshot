d00b4bbb1162d0953aed49e3720961b2c8324789
Raphael Reyna <raphaelreyna@protonmail.com>
2020-07-05 19:20:35 -0700

    First commmit

--- goneshot/MainWindow.xaml.cs
+++ goneshot/MainWindow.xaml.cs
@@ -21,22 +21,49 @@ namespace goneshot
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string port;
+        private string timeOut;
+        private bool noDownload;
+        private bool exitOnFial;
+        private string archiveMethod;
+        private bool mdns;
+        private string fileName;
+        private string fileExt;
+        private string fileMime;
+        private string certFile;
+        private string keyFile;
+        private bool ssTLS;
+        private string username;
+        private string password;
+        private bool cgi;
+        private bool cgiStrict;
+        private string shellCommand;
+        private bool replaceHeaders;
+        private string dir;
+
         public MainWindow()
         {
             InitializeComponent();
+            run();
+        }
 
-            var process = new Process();
-            process.StartInfo.FileName = "C:\\Users\\r\\oneshot\\oneshot.exe";
-            process.StartInfo.Arguments = "-u C:\\Users\\r\\Desktop";
-            process.StartInfo.UseShellExecute = true;
-            process.Start();
-
-            //this.Close();
+        private void downloadChecked(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
+        }
+
+        private void run()
+        {
+            var process = new Process();
+            process.StartInfo.FileName = "C:\\Tools\\goneshot\\oneshot.exe";
+            process.StartInfo.Arguments = "-u";
+            process.StartInfo.UseShellExecute = true;
+            process.Start();
         }
     }
 }
